35ff2a558938fe028cf362e45141b386135dffdf
Dane Dalton <danedalton@sbcglobal.net>
2022-06-08 15:18:27 -0500

    Add function for getting the value of the color selected. Add clear button

--- script.js
+++ script.js
@@ -1,40 +1,52 @@
 const etchASketch = document.querySelector('.etch-a-sketch')
 const submit = document.querySelector('.submit')
 const gridSizeForm = document.querySelector('.grid-size')
+const clearBtn = document.querySelector('.btn-clear')
+const colorBtns = document.querySelectorAll('.btn-color')
+
 const warningMsg = document.createElement('div')
 
 const defaultGridSize = 16
 
 //Create grid
 window.onload = createEtchGrid(defaultGridSize)
+let selectedColor = 'black'
 
-drawOnGrid()
+selectedColor = selectColor()
+
+createNewGrid()
+clearGrid()
+drawOnGrid(selectedColor)
+
+
 
 //Grid size submissions
-submit.addEventListener('click', () => {
+function createNewGrid() {
+    submit.addEventListener('click', () => {
 
-    const input = document.querySelector('.input')
-    const inputText = input.value
-    
-    //remove error message if it exists
-    if ( gridSizeForm.lastElementChild == warningMsg) gridSizeForm.removeChild(warningMsg)
-
-    //Input validation w/ warning message
-    if ( isNaN(inputText) || inputText < 1 || inputText > 100) {
-        warningMsg.textContent = 'Oops! You must enter a number between 1 and 100 to create a grid.'
+        const input = document.querySelector('.input')
+        const inputText = input.value
         
-        warningMsg.classList.add('warning-msg')
-        gridSizeForm.appendChild(warningMsg)
-        return;
-    }
+        //remove error message if it exists
+        if ( gridSizeForm.lastElementChild == warningMsg) gridSizeForm.removeChild(warningMsg)
+    
+        //Input validation w/ warning message
+        if ( isNaN(inputText) || inputText < 1 || inputText > 100) {
+            warningMsg.textContent = 'Oops! You must enter a number between 1 and 100 to create a grid.'
+            
+            warningMsg.classList.add('warning-msg')
+            gridSizeForm.appendChild(warningMsg)
+            return;
+        }
+    
+        const size = inputText
+    
+        createEtchGrid(size)
 
-    const size = inputText
-
-    createEtchGrid(size)
-    drawOnGrid()
-})
-
-
+        drawOnGrid()
+        clearGrid()
+    })
+}
 
 //Clear and create new grid
 function createEtchGrid(size) {
@@ -74,7 +86,22 @@ function drawOnGrid() {
 }
 
 //Clear button empties grid colors
+function clearGrid() {
+    const gridItems = document.querySelectorAll('.grid-item')
 
-
+    clearBtn.addEventListener('click', () => {
+        gridItems.forEach((gridItem) => {
+            gridItem.style.backgroundColor = 'white'
+            gridItem.style.opacity = '1'
+        })
+    })
+}
 
 //Clicking a new color changes sketch color
+function selectColor() {
+    colorBtns.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            return e.target.value
+        })
+    })
+}
